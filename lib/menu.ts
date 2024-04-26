c84b27cae966bb004ad40fc25f5911caedf35519
Bianca Chami <b.chami39@gmail.com>
2024-04-26 13:06:50 +0100

    feat: updated footer and nav

--- lib/menu.ts
+++ lib/menu.ts
@@ -8,6 +8,8 @@ import {
   ZendeskCategory,
 } from '@ircsignpost/signpost-base/dist/src/zendesk';
 
+import { COUNTRY_SELECTOR, CountrySelectorInterface } from './constants';
+
 export interface CustomMenuOverlayStrings extends MenuOverlayStrings {
   information: string;
   about: string;
@@ -19,7 +21,42 @@ export function getFooterItems(
   strings: CustomMenuOverlayStrings
 ): MenuOverlayItem[] {
   let items: MenuOverlayItem[] = [];
-  items.push({ key: 'home', label: strings.home, href: '/' });
+  items.push({
+    key: 'mapa-servicios',
+    label: 'Mapa de servicios del GIFMM',
+    href: 'https://mapeo-de-servicios.gifmm-colombia.site/',
+  });
+  items.push({
+    key: 'gifmm-contigo',
+    label: 'GIFMM Contigo',
+    href: 'https://gifmm-contigo.com/ ',
+  });
+  items.push({
+    key: 'ecuador',
+    label: 'InfoPalante Ecuador',
+    href: 'https://www.infopalanteec.org/',
+  });
+  items.push({
+    key: 'centroamerica',
+    label: 'Centroamérica: Cuéntanos ',
+    href: 'https://www.cuentanos.org/',
+  });
+  items.push({
+    key: 'México',
+    label: 'México: InfoDigna',
+    href: 'https://www.infodigna.org',
+  });
+
+  items.push({
+    key: 'ee-uu',
+    label: 'EE.UU: ImportaMi',
+    href: 'https://www.importami.org/',
+  });
+  items.push({
+    key: 'Colombia',
+    label: 'Colombia: InfoPalante',
+    href: 'https://www.infopalante.org/es',
+  });
   return items;
 }
 
@@ -34,5 +71,18 @@ export function getMenuItems(
     label: strings.services,
     href: '/#service-map',
   });
+  addCountrySelector(items, COUNTRY_SELECTOR);
   return items;
+}
+function addCountrySelector(
+  items: MenuOverlayItem[],
+  countries: CountrySelectorInterface[]
+) {
+  items.push({
+    key: 0,
+    label: 'Peru',
+    children: countries.map((country) => {
+      return { key: country.title, label: country.title, href: country.url };
+    }),
+  });
 }
